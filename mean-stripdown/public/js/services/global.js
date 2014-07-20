d81676cfd6b9f5e42465d8b5d83d6fb00dea6316
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-20 17:43:10 -0400

    Implementing service for returning user object

--- mean-stripdown/public/js/services/global.js
+++ mean-stripdown/public/js/services/global.js
@@ -1,4 +1,17 @@
+/* 
+ The global service will return an object which we can use to identify 
+ the user, as well as to determine if that user has logged in or not. 
+*/
 window.angular.module('ngff.services.global', [])
   .factory('Global', function() {
+    var current_user = window.user;
+    return {
+        currentUser: function(){
+            return current_user;
+        },
+        isSignedIn: function(){
+            return !!current_user;
+        }
+    };
 
   });

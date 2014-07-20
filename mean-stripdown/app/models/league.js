3af9d219d2efb950c091e385d4dba263e2dc7f6e
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-20 18:13:53 -0400

    Defining statistics method for League model

--- mean-stripdown/app/models/league.js
+++ mean-stripdown/app/models/league.js
@@ -7,3 +7,11 @@ var LeagueSchema = new Schema({
     name: {type: String},
     commissioner: {type: Schema.ObjectId, ref: 'User'}
 })
+
+LeagueSchema.statics = {
+    load: function (id, cb) {
+        this.findOne({ _id : id }).populate('commissioner').exec(cb);
+    }
+};
+
+mongoose.model('League', LeagueSchema);

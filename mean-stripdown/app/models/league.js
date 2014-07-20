d06399488f1f0408042a77cc8977bc5ed24486d1
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-20 18:12:12 -0400

    Adding league mongoose model

---
+++ mean-stripdown/app/models/league.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose'),
+    env = process.env.NODE_ENV || 'development',
+    config = require('../../config/config')[env],
+    Schema = mongoose.Schema;
+
+var LeagueSchema = new Schema({
+    name: {type: String},
+    commissioner: {type: Schema.ObjectId, ref: 'User'}
+})

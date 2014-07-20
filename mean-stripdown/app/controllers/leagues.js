da857856f81e020204899baf48b502d0a89b297c
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-20 18:17:04 -0400

    Adding create() method for leagues

--- mean-stripdown/app/controllers/leagues.js
+++ mean-stripdown/app/controllers/leagues.js
@@ -2,3 +2,10 @@ var mongoose = require('mongoose'),
     async = require('async'),
     League = mongoose.model('League'),
     _ = require('underscore')
+
+exports.create = function(req, res){
+    var league = new League(req.body);
+    league.commissioner = req.user;
+    league.save();
+    res.jsonp(league);
+}

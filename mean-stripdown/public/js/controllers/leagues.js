4e4b8e4c063eebf186ab0e7a32c5821ba4f071bc
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-21 23:39:22 -0400

    Adding create method for league controller

--- mean-stripdown/public/js/controllers/leagues.js
+++ mean-stripdown/public/js/controllers/leagues.js
@@ -2,4 +2,16 @@ window.angular.module('ngff.controllers.leagues', [])
     .controller('LeaguesController', ['$scope','$routeParams','$location','Global','Leagues',
         function ($scope, $routeParams, $location, Global, Leagues) {
             $scope.global = Global;
+
+        $scope.create = function () {
+            var league = new Leagues({
+            name: this.league.name
+        });
+         
+        league.$save(function (response) {
+            $location.path("leagues/" + response._id);
+        });
+         
+            this.league.name = "";
+        };
 }]);

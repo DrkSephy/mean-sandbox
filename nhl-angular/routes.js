044eab4a4a05df94f7a7decd4190402793e9f67f
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-23 22:17:08 -0400

    Adding helloworld route

--- nhl-angular/routes.js
+++ nhl-angular/routes.js
@@ -1,4 +1,12 @@
-module.exports = function(router){
+module.exports = function(router, request){
     // The Router handles any requests within this endpoint
     // depending on where router is used. 
+    router.get('/helloworld', function(req, res){
+        var url = 'http://nhlwc.cdnak.neulion.com/fs1/nhl/league/teamroster/PIT/iphone/clubroster.json';
+        request(url, function(error, response, body){
+            if(!error && response.statusCode === 200){
+                res.send(body);
+            }
+        });
+    });
 }
